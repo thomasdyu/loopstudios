22ee438be6b9a0bfdbff52bfb3f64aaaf43bc237
Tom <thomasdyu@hotmail.com>
2021-03-11 16:31:06 -0600

    fixed mobile view

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,22 +3,24 @@ import mainImg from '../images/desktop/image-interactive.jpg';
 import './MainContent.css';
 const Main = () => {
     return (
-        <main className='main container'>
-            <div className='img-container'>
-                <img className='main__img' src={mainImg} alt='Hero' />
-            </div>
+        <main className='container'>
+            <div className='content'>
+                <div className='img-container'>
+                    <img className='main__img' src={mainImg} alt='Hero' />
+                </div>
 
-            <div className='main__text-container'>
-                <h1 className='main__text-heading'>
-                    the leader in interactive vr
-                </h1>
-                <p className='main__text-paragraph'>
-                    Founded in 2011, Loopstudios has been producing world-class
-                    virtual reality projects for some of the best companies
-                    around the globe. Our award-winning creations have
-                    transformed businesses through digital experiences that bind
-                    to their brand.
-                </p>
+                <div className='main__text-container'>
+                    <h1 className='main__text-heading'>
+                        the leader in interactive vr
+                    </h1>
+                    <p className='main__text-paragraph'>
+                        Founded in 2011, Loopstudios has been producing
+                        world-class virtual reality projects for some of the
+                        best companies around the globe. Our award-winning
+                        creations have transformed businesses through digital
+                        experiences that bind to their brand.
+                    </p>
+                </div>
             </div>
         </main>
     );
