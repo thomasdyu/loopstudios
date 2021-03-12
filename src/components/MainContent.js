23d89f4449aea1ddcbc214b7510aa19f1c34bf15
Tom <thomasdyu@hotmail.com>
2021-03-12 12:53:54 -0600

    bug fix on main content layout v1

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,26 +3,26 @@ import mainImg from '../images/desktop/image-interactive.jpg';
 import './MainContent.css';
 const Main = () => {
     return (
-        <div className='container'>
-            <div className='content'>
-                <div className='img-container'>
-                    <img className='main__img' src={mainImg} alt='Hero' />
-                </div>
+        // <div className='container'>
+        <div className='content'>
+            <div className='img-container'>
+                <img className='main__img' src={mainImg} alt='Hero' />
+            </div>
 
-                <div className='main__text-container'>
-                    <h1 className='main__text-heading'>
-                        the leader in interactive vr
-                    </h1>
-                    <p className='main__text-paragraph'>
-                        Founded in 2011, Loopstudios has been producing
-                        world-class virtual reality projects for some of the
-                        best companies around the globe. Our award-winning
-                        creations have transformed businesses through digital
-                        experiences that bind to their brand.
-                    </p>
-                </div>
+            <div className='main__text-container'>
+                <h1 className='main__text-heading'>
+                    the leader in interactive vr
+                </h1>
+                <p className='main__text-paragraph'>
+                    Founded in 2011, Loopstudios has been producing world-class
+                    virtual reality projects for some of the best companies
+                    around the globe. Our award-winning creations have
+                    transformed businesses through digital experiences that bind
+                    to their brand.
+                </p>
             </div>
         </div>
+        // </div>
     );
 };
 
