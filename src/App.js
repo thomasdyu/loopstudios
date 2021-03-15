a84ee7415700ac079d7970ec3b346eea347dde60
Tom <thomasdyu@hotmail.com>
2021-03-15 13:55:50 -0500

    text5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Header from './components/Header';
-// import MainContent from './components/MainContent';
+import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
 import './App.css';
-import Test from './components/Test';
 
 function App() {
     return (
         <div>
             <Header />
-            {/* <MainContent /> */}
-            <Test />
+            <MainContent />
             <Gallery />
             <Footer />
         </div>
