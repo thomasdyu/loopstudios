b126ed572b7b442885ff771a7348ccd2b02a9dd4
Tom <thomasdyu@hotmail.com>
2021-03-15 23:07:12 -0500

    test 21

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,20 +4,22 @@ import './MainContent.css';
 const MainContent = () => {
     return (
         <div className='container'>
-            <div className='content'>
-                <img className='main__img' src={mainImg} alt='Hero' />
+            <div>
+                <div className='content'>
+                    <img className='main__img' src={mainImg} alt='Hero' />
 
-                <div className='main__text-container'>
-                    <h1 className='main__text-heading'>
-                        the leader in interactive vr
-                    </h1>
-                    <p className='main__text-paragraph'>
-                        Founded in 2011, Loopstudios has been producing
-                        world-class virtual reality projects for some of the
-                        best companies around the globe. Our award-winning
-                        creations have transformed businesses through digital
-                        experiences that bind to their brand.
-                    </p>
+                    <div className='main__text-container'>
+                        <h1 className='main__text-heading'>
+                            the leader in interactive vr
+                        </h1>
+                        <p className='main__text-paragraph'>
+                            Founded in 2011, Loopstudios has been producing
+                            world-class virtual reality projects for some of the
+                            best companies around the globe. Our award-winning
+                            creations have transformed businesses through
+                            digital experiences that bind to their brand.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
